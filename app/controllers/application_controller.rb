770c319f620b6adbe0ce4fea8b665376f8a3ba71
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 22:38:29 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,7 @@ class ApplicationController < Sinatra::Base
       session[:id] = @user.id
       redirect '/account'
     else
-      render "Incorrect password!"
+      "Incorrect password!"
     end
   end
 
