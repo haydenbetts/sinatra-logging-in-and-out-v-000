4c1d5d1109d9a65cf570ac392a45c883819674a3
Hayden Betts <haydenbetts@gmail.com>
2018-07-14 22:36:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,8 @@ class ApplicationController < Sinatra::Base
   post '/login' do
     if @user = User.find_by(username: params[:username], password: params[:password])
       session[:id] = @user.id
+      redirect '/account'
+    else
   end
 
   get '/account' do
